8f306b2ef804674acc16d00b3319d77d4f619869
Prince Thapaliya <prince.thapaliya69@gmail.com>
2026-05-26 08:40:58 +0545

    add session_start to confirm_booking

--- confirm_booking.php
+++ confirm_booking.php
@@ -1,10 +1,14 @@
+<?php 
+  session_start();
+  require('inc/links.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?php require('inc/links.php'); ?>
   <!-- Add Pannellum CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.css">
   <title><?php echo $settings_r['site_title'] ?> - CONFIRM BOOKING</title>
