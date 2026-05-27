9e5b55ccc7f02fdde229103386a04627dd6e5d24
Prince Thapaliya <prince.thapaliya69@gmail.com>
2026-05-27 22:49:14 +0545

    fixed booking records page of admin it was showing html tags instead of acutal data v2

--- admin/scripts/booking_records.js
+++ admin/scripts/booking_records.js
@@ -4,83 +4,67 @@ function release_room(booking_id)
         return;
     }
 
-    let xhr = new XMLHttpRequest();
-
-    xhr.open('POST', 'ajax/booking_records.php', true);
-
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-    xhr.onload = function ()
-    {
-        let res = JSON.parse(this.responseText);
-
-        if (res.success)
-        {
-            alert(res.message);
-
-            get_bookings(
-                document.getElementById('search_input').value
-            );
+    fetch('ajax/booking_records.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: 'release_booking=1&booking_id=' + booking_id
+    })
+    .then(response => response.json())
+    .then(res => {
+        alert(res.message);
+        if (res.success) {
+            get_bookings(document.getElementById('search_input').value);
         }
-        else
-        {
-            alert(res.message);
-        }
-    };
-
-    xhr.send(
-        'release_booking=1&booking_id=' + booking_id
-    );
+    })
+    .catch(err => {
+        console.error(err);
+        alert('Something went wrong!');
+    });
 }
-
 
 function get_bookings(search = "", page = 1)
 {
     let xhr = new XMLHttpRequest();
 
     xhr.open("POST", "ajax/booking_records.php", true);
-
-    xhr.setRequestHeader(
-        "Content-Type",
-        "application/x-www-form-urlencoded"
-    );
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
     xhr.onload = function ()
     {
-        let data = JSON.parse(this.responseText);
+        try {
+            let data = JSON.parse(this.responseText);
 
-        document.getElementById("table-data").innerHTML =
-            data.table_data;
+            document.getElementById("table-data").innerHTML = 
+                data.table_data || "<tr><td colspan='6' class='text-center py-4'>No Data Found!</td></tr>";
 
-        document.getElementById("pagination").innerHTML =
-            data.pagination;
+            if (data.pagination) {
+                document.getElementById("pagination").innerHTML = data.pagination;
+            }
+        } catch(e) {
+            console.error("Response parsing error:", e);
+            document.getElementById("table-data").innerHTML = 
+                "<tr><td colspan='6' class='text-center text-danger'>Error loading bookings</td></tr>";
+        }
     };
 
     xhr.send(
-        "get_bookings=1&search=" +
-        encodeURIComponent(search) +
-        "&page=" + page
+        "get_bookings=1&search=" + encodeURIComponent(search) + "&page=" + page
     );
 }
-
 
 function change_page(page)
 {
-    get_bookings(
-        document.getElementById('search_input').value,
-        page
-    );
+    get_bookings(document.getElementById('search_input').value, page);
 }
-
 
 function download(id)
 {
-    window.location.href =
-        'generate_pdf.php?gen_pdf&id=' + id;
+    window.location.href = 'generate_pdf.php?gen_pdf&id=' + id;
 }
-
 
 window.onload = function ()
 {
     get_bookings();
-}
+};
