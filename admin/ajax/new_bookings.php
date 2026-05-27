5a826fe16d586b764f7132fb4f628c6b59310bd4
Prince Thapaliya <prince.thapaliya69@gmail.com>
2026-05-27 23:56:39 +0545

    updated booking_records.js v2

--- admin/ajax/new_bookings.php
+++ admin/ajax/new_bookings.php
@@ -12,7 +12,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Admin Panel - New Bookings</title>
-  <?php require('inc/links.php'); ?>
+  <?php require(__DIR__ .'inc/links.php'); ?>
 </head>
 <body class="bg-light">
 
