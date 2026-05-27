c03289b001c73c55cd18a9b05b12b41cd0ff3b4c
Prince Thapaliya <prince.thapaliya69@gmail.com>
2026-05-27 23:21:56 +0545

    fixed booking records page of admin it was showing html tags instead of acutal data v5

--- admin/ajax/booking_records.php
+++ admin/ajax/booking_records.php
@@ -1,8 +1,8 @@
 <?php 
 ob_start();
-require_once(__DIR__ . '/../inc/db_config.php');
-require_once(__DIR__ . '/../inc/essentials.php');
-require_once(__DIR__ . '/../../inc/room_availability.php');
+require_once('/../inc/db_config.php');
+require_once('/../inc/essentials.php');
+require_once('/../../inc/room_availability.php');
 
 date_default_timezone_set("Asia/Kathmandu");
 adminLogin();
